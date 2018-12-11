145cfbc405e20d7fc8d186d266c04c5b6bc10382
DenizArasK <denizarask@hotmail.com>
2018-12-11 13:57:55 +0100

    test1

---
+++ test.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class test{
+
+boolean test = true;
+
+public static ArrayList<String> testTicketName =
+        new ArrayList<String>(Arrays.asList(
+                "Barn",
+                "Ung vuxen",
+                "Vuxen",
+                "Senior",
+                "Familj"));
+
+public static void main(String[] args) {
+
+for(int i=0; i < testTicketName(i); i++){
+test = (ticketName.get(i) == testTicketName(i));
+}
+return((ticketName.get(0) == testTicketName(0)));
+
+
+}
+
+
+
+
+
+
+}
