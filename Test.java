467b30c1cccef81bd525b1b0fab588db95643f76
AldoRiz <aldoruiz342@hotmail.com>
2018-12-11 13:28:33 +0100

    Påbörjar Test

---
+++ Test.java
@@ -0,0 +1,25 @@
+public class Test {
+
+private static TestTis_Tickets test = new TestTis_Tickets();
+
+private static boolean ticketName(){
+test.reset();
+return barn== test.ticketName;
+
+
+}
+
+  public static void main(String[] args){
+    if(ticketName()){
+      System.out.printf("Barn", "PASSED");
+    } else {
+      System.out.printf("Barn", "FAILED");
+
+    }
+
+
+
+
+  }
+
+}
