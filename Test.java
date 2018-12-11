3bb7703c7b7d9b227ee607aaa519ce6ef79fd698
DenizArasK <denizarask@hotmail.com>
2018-12-11 14:38:24 +0100

    testmetod

--- Test.java
+++ Test.java
@@ -1,18 +1,24 @@
+import java.util.ArrayList;
+
+public class test{
+
+public boolean test = true;
+
 public static ArrayList<String> testTicketName =
-        new ArrayList<String>(Arrays.asList(
-                "Barn",
-                "Ung vuxen",
-                "Vuxen",
-                "Senior",
-                "Familj"));
-  "Barn","Ung vuxen","Vuxen","Senior","Familj"));
- public static void main(String[] args) {
- for(int i=0; i < testTicketName(i); i++){
-test = (ticketName.get(i) == testTicketName(i));
-  test = (ticketName.get(i) == testTicketName.get(i));
-  if (test == false) {
-    break;
-  }
+       new ArrayList<String>(Arrays.asList(
+ “Barn”,“Ung vuxen”,“Vuxen”,“Senior”,“Familj”));
+
+
+
+
+public static void main(String[] args) {
+
+for(int i=0; i < testTicketName.get(i); i++){
+test = (ticketName.get(i) == testTicketName.get(i));
+ if (test == false) {
+   break;
+ }
 }
-return((ticketName.get(0) == testTicketName(0)));
-//return test;
+return test;
+
+}
